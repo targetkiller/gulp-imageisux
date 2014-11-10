3312c72a7ed06b5ecd7fa057a187b40aae70acc8
targetkiller <369160020@qq.com>
2014-11-10 16:18:08 +0800

    增加option配置
    允许设置保存压缩后图片的路径，允许设置是否吐回webp。

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ var needle = require('needle');
 var path = require('path');
 var fs = require('fs');
 
-function imageisux (argument) {
-	var stream = through.obj(function(file,encoding,callback){
+function imageisux (abspath,enableWebp) {
+    var stream = through.obj(function(file,encoding,callback){
+        abspath = abspath || "";
+        enableWebp = enableWebp || false;
         var _that = this;
     	if (file.isNull()) {
             this.push(file);
@@ -35,75 +37,114 @@ function imageisux (argument) {
                 needle.post('http://image.isux.us/index.php/preview/upload_file', data, {multipart:true}, function(err, resp ,body) {
                     if(err){
                         console.error('send post error:' + err);
-                    }else{
+                    }
+                    else{
                         console.log('photo receive...');
-                        if(body.indexOf('{')!==-1){
+                        if(body.indexOf('{')>-1){
                             // turn the data to json
                             var str = '({'+body.split('{')[1]+')';
-                            var json_str = eval(str);
+                            try{
+                                var json_str = eval(str);
 
-                            // 得到输出的图片
-                            var output = json_str.output;
-                            var output_png = json_str.output_png;
-                            var output_webp = json_str.output_webp;
-                            var size = json_str.size;
+                                // get the url of image
+                                var output = json_str.output;
+                                var output_webp = json_str.output_webp;
 
-                            /*
-                            * all the images return
-                            * type=1：origin
-                            * type=2：png
-                            * type=3：webp
-                            */
-                            var output_ary = new Array();
-                            output_ary.push({'type':1,'url':output});
-                            if(output_png!==undefined){
-                                output_ary.push({'type':2,'url':output_png});
-                            }
-                            if(output_webp!==undefined){
-                                output_ary.push({'type':3,'url':output_webp});
-                            }
+                                // var output_png = json_str.output_png;
+                                // var size = json_str.size;
 
-                            var DEST_DIR = file_dirname+"/dest";
-                            var FILE_CONTENT = file_name.split('.');
-                            var FILENAME = FILE_CONTENT[0];
-                            var FILETYPE = FILE_CONTENT[1];
-
-                            // make dir
-                            fs.exists(DEST_DIR,function(exists){
-                                if(!exists){
-                                    fs.mkdirSync(DEST_DIR,0777);
-                                }
-                            });
-                            
-                            // download the image from ZHITU
-                            for(var i = 0; i < output_ary.length; i++){
-                                (function(){
-                                    var PREFIX = "";
-                                    var TYPE = FILETYPE;
-                                    switch(output_ary[i].type){
-                                        case 1: PREFIX = "";TYPE = '.'+FILETYPE;break;
-                                        case 2: PREFIX = "png_";TYPE = ".png";break;
-                                        case 3: PREFIX = "webp_";TYPE = ".webp";break;
-                                        default:PREFIX="";break;
+                                /*
+                                * all the images return
+                                * type=1：origin
+                                * type=2：webp
+                                */
+                                var output_ary = new Array();
+                                // abspath is exist, so just give the webp.
+                                if(abspath!==""&&enableWebp===true){
+                                    if(output_webp!==undefined){
+                                        output_ary.push({'type':2,'url':output_webp});
                                     }
-                                    
-                                    needle.get(output_ary[i].url, function(err, resp, body) {
-                                        if(body) {
-                                            var fd = DEST_DIR + "/" + PREFIX + FILENAME + TYPE;
-                                            fs.writeFile(fd, body, function(err, data){
-                                                if (err) {
-                                                  gutil.log('[error] :   ', err);
-                                                }
-                                            });
-                                        } else {
-                                            gutil.log('[error] : image does not exist!');
+                                    else{
+                                        gutil.log('[error]','this webp is not exist!'+file_name);
+                                    }
+                                }
+                                // abspath is exist and enableWebp is false,so give the origin.
+                                else if(abspath!==""&&enableWebp===false){
+                                    if(output!==undefined){
+                                        output_ary.push({'type':1,'url':output});
+                                    }
+                                    else{ 
+                                        gutil.log('[error]','this origin is not exist!'+file_name);
+                                    }
+                                }
+                                // abspath is not exist, so init /dest/ and /webp/ and give the origin and webp types.
+                                else if(abspath===""){
+                                    if(output!==undefined){
+                                        output_ary.push({'type':1,'url':output});
+                                    }
+                                    if(output_webp!==undefined){
+                                        output_ary.push({'type':2,'url':output_webp});
+                                    }
+                                }
+
+                                // console.log(output_ary);
+
+                                var FILE_CONTENT = file_name.split('.');
+                                var FILENAME = FILE_CONTENT[0];
+                                var FILETYPE = FILE_CONTENT[1];
+
+                                // download the image from server http://image.isux.us
+                                for(var i = 0; i < output_ary.length; i++){
+                                    (function(){
+                                        var PREFIX = "";
+                                        var APPENDFIX = "."+FILETYPE;
+                                        var OUTPUT_TYPE = output_ary[i].type;
+                                        switch(OUTPUT_TYPE){
+                                            case 1: PREFIX = "";APPENDFIX = '.'+FILETYPE;break;
+                                            case 2: PREFIX = "";APPENDFIX = ".webp";break;
+                                            default:PREFIX="";break;
                                         }
-                                    });
-                                })(i);
+                                        
+                                        needle.get(output_ary[i].url, function(err, resp, body) {
+                                            if(body) {
+                                                if(abspath){
+                                                    var DEST_DIR = file_dirname + "/" + abspath + "/";
+                                                }
+                                                else if(OUTPUT_TYPE==1){
+                                                    var DEST_DIR = file_dirname + "/dest/";
+                                                }
+                                                else if(OUTPUT_TYPE==2){
+                                                    var DEST_DIR = file_dirname + "/webp/";
+                                                }
+
+                                                var fd = DEST_DIR + PREFIX + FILENAME + APPENDFIX;
+
+                                                // make dir
+                                                fs.exists(DEST_DIR,function(exists){
+                                                    if(!exists){
+                                                        fs.mkdirSync(DEST_DIR);
+                                                    }
+                                                });
+
+                                                fs.writeFile(fd, body, function(err, data){
+                                                    if (err) {
+                                                      gutil.log('[error] :   ', err);
+                                                    }
+                                                });
+                                            } else {
+                                                gutil.log('[error]','return data is not exist!'+FILENAME);
+                                            }
+                                        });
+                                    })(i);
+                                }
                             }
+                            catch(err){
+                                gutil.log('[error]',err+" filename:"+ file_name);
+                            }
+
                         }
                         else{
-                            gutil.log('[error] : result can not be empty!');
+                            gutil.log('[error]','result can not be empty!'+file_name);
                         }
                     }
                 });
